5a959b7b2854ae6fe3acd743df4c6b6251adffc5
dlt-v <tmm.michalski@gmail.com>
2023-10-07 15:44:54 +0200

    zadanie

---
+++ 04 - operatory logiczne/main.py
@@ -0,0 +1,50 @@
+"""
+> - większy od
+< - mniejszy od
+
+>= - większy bądź równy
+<= - mniejszy bądź równy
+
+== - równy 
+!= - nierówny
+
+and - i
+or  - lub
+not - zaprzeczenie
+
+
+Tworzenie zapytań/kwarend
+
+a > 10 - "czy a jest większe od 10?" - True albo False
+
+Wartość boolowska jest wynikiem tego zapytania.
+
+and, or, not - pozwalają nam na tworzenie złożonych zapytań logicznych.
+
+"""
+
+pieniadze = 10
+wiek = 14
+
+# Czy mamy więcej niż 7zł? Oraz czy mamy 16 bądź więcej lat.
+wynik = pieniadze >= 7 and wiek >= 16
+#          True      and      False
+
+# AND - zwraca True jeżeli wszystkie zapytania są prawdziwe. 
+# Jeżeli przynajmniej jeden z nich jest nieprawdziwy to zwraca False.
+# np. TRUE and TRUE and TRUE and FALSE => FALSE
+
+# OR - zwraca True jeżeli przynajmniej jeden warunek jest prawdziwy.
+# np. TRUE or FALSE or FALSE or FALSE => TRUE
+
+# NOT - zaprzeczenie albo po prostu odwrotność wyrażenia
+# np. not True => False | not False => True
+
+
+print(f"Czy możemy wejść do kina na horror? {wynik}")
+
+print(f'False and True: {False and True}')
+print(f'False or True: {False or True}')
+print(f'not True: {not True}')
+print(f'not False: {not False}')
+
---
+++ 04 - operatory logiczne/zadania.py
@@ -0,0 +1,10 @@
+'''
+Wybierz i uzupełnij jeden (lub więcej) z operatorów logicznych podanych w komentarzu,
+aby uzyskać oczekiwany wynik.
+
+'''
+print(12 15) # Fałsz # ==, <, !=, <=
+print(5 15000) # Prawda # ==, >, >=, <=
+print(120 120) # Fałsz # ==, >=, !=, <=
+print(60 15) # Fałsz # ==, <, !=, >=
+print(25.3421 25.3421) # Prawda # ==, <, !=, <=
