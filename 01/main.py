8d4513487ef3c156b62fb80b9f405ab8c0bec1df
dlt-v <tmm.michalski@gmail.com>
2023-10-07 11:17:28 +0200

    update

--- 01/main.py
+++ 01/main.py
@@ -29,16 +29,25 @@
 # print(napis_na_liczbe + 20)
 # print(liczba_na_napis + "abc")
 
-wiek = input("Jaki masz wiek: ")
-print("Zmienna wiek ma typ: ", type(wiek)) # str - string
-wiek = int(wiek) # konwertujemy na liczbę
-wiek = wiek + 5
-# print("Za 5 lat będziesz miał",wiek ,"lat.")
+# wiek = input("Jaki masz wiek: ")
+# print("Zmienna wiek ma typ: ", type(wiek)) # str - string
+# wiek = int(wiek) # konwertujemy na liczbę
+# wiek = wiek + 5
+# # print("Za 5 lat będziesz miał",wiek ,"lat.")
 
-print("Zmienna wiek ma typ: ", type(wiek)) # int - integer - liczba całkowita
+# print("Zmienna wiek ma typ: ", type(wiek)) # int - integer - liczba całkowita
 
 # Zadanie:
 # Pobierz od użytkownika imię, wiek i klasę. Wydrukuj potem te dane w jednej linijce.
 # 5 min - 11:05.
 
+imie = input("Podaj swoje imię: ")
+wiek = input("Podaj swój wiek: ") # UWAGA - to będzie napis
+klasa = input("Podaj swoją klasę: ")
 
+print("Masz na imię ", imie, "Twój wiek to ", wiek, "Jesteś w klasie", klasa)
+
+# f-string - lepszy sposób na formatowanie tekstu.
+# Połączenie tesktu z wartościami ze zmiennych nazywany INTERPOLACJĄ.
+
+print(f"Masz na imię {imie}. Twój wiek to {wiek} i jesteś w klasie {klasa}.")
