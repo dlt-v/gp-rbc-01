f98bb308d81fa2ea2d1a0e40a986e29c6d7e5989
dlt-v <tmm.michalski@gmail.com>
2023-10-07 12:30:16 +0200

    add tasks

---
+++ 02 - arytmetyka/konwencje_nazewnictwa.py
@@ -0,0 +1,20 @@
+# Zasady:
+
+# 1. Nie używamy znaków specjalnych. ąś
+
+# 2. Nazwy zmiennych najczęściej nazywamy po tym co one zawierają. wiek = 25, imie = "Tomek"
+# 2.5. Tak samo funkcje i metody, nazywamy po tym co one robią. dodaj_liczby()
+
+# 3. Przy nazywaniu zmiennych, funkcji używamy snake_case (same małe litery, i słowa oddzielane są _ np. lista_studentow, interpretuj_dane() )
+
+# PascalCase - wszystkie słowa są kapilozowane i nie mają spacji - ListaStudentow
+# CamelCase - wszystkie słowa (oprócz pierwszego) są kapilozowane i nie mają spacji = listaStudentow
+
+# Komentarze jednoliniowe są pisane po symbolu #
+
+print("Hello World") # To jest również komentarz
+
+'''
+    To jest komentarz wieloliniowy.
+    Użycie tego komentarza jest najczęściej używane to opisywania funkcjonalności pewnego fragmentu kodu. Np. Opisuje co robi dana funkcja.
+'''
---
+++ 02 - arytmetyka/zadania.py
@@ -0,0 +1,17 @@
+"""
+    Zadanie 1:
+    Pobierz od użytkownika (input) boki prostokąta i oblicz pole i obwód i wyświetl w terminalu.
+    Pamiętaj że input zwraca napis, czyli trzeba go przekonwertować.
+    Do 12:20.
+    
+    bok_a = input("Podaj bok a:" )
+    bok_a = int(bok_a)
+
+    Zadanie 2:
+    Poproś użytkownika o podanie temperatury w stopniach Celsjusza i przelicz ją na stopnie Fahrenheita za pomocą wzoru: F = (C * 9/5) + 32 oraz wyświetl wynik w terminalu.
+    Do 12:25.
+
+    Zadanie 3:
+    Poproś użytkownika o podanie trzech ocen szkolnych i oblicz ich średnią arytmetyczną. Wyświetl wynik.
+    Do 12:30.
+"""
