18575c593bfc669f1cb65b6f194f11881e3f1882
dlt-v <tmm.michalski@gmail.com>
2023-10-07 11:57:53 +0200

    update

--- 02 - arytmetyka/arytmetyka.py
+++ 02 - arytmetyka/arytmetyka.py
@@ -2,11 +2,21 @@
 
 # Działania na liczbach całkowitych i zmiennoprzecinkowych
 
-a = 5
-b = 2
+a = 7
+b = 5
 
 print(f"Dodawanie: {a} + {b} = {a + b}")
 print(f"Odejmowanie: {a} - {b} = {a - b}")
 print(f"Mnożenie: {a} * {b} = {a * b}")
 print(f"Dzielenie: {a} / {b} = {a / b}") # Wynikiem dzielenia jest zawsze Float
+print(f"Dzielenie całkowite: {a} // {b} = {a // b}") 
+print(f"Modulo | Reszta z dzielenia: {a} % {b} = {a % b}")
+
+# Modulo - jak działa
+# 5 % 2
+# Ile pełnych dwójek znajduje się w 5? 2 dwójki.
+# Co nam zostaje jeżeli te pełne liczby usuniemy? 1
+
+# 7 % 5 = ? 2
+
 
