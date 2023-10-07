8be8643045a342a455af76723c152e828b1942e1
dlt-v <tmm.michalski@gmail.com>
2023-10-07 10:38:27 +0200

    update

--- 01/main.py
+++ 01/main.py
@@ -1,7 +1,16 @@
-print("Cześć świecie!")
+# # Struktura danych
 
-# Zadanie:
-# Wydrukuj własne imię.
+# imie = "Tomek" # Napis / String
 
-print("Cześć Tomek")
-print("Czesc Paulina")
+# # Typy Danych (Prymitywne)
+
+# wiek = 23 # Liczba Całkowita / Integer / Int
+# wzrost = 1.80 # Liczba Zmiennoprzecinkowa / Floating Point Number / Float
+# bool = True # False / Bool
+
+# Wprowadzanie danych za pomocą terminala
+
+print("Jak masz na imię? ") # Drukowanie do terminala
+imie = input() # Pobieranie informacji z terminala
+
+print("Cześć " + imie)
