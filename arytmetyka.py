a0c7c28db28ee87172c805d2ff856ad60003da81
dlt-v <tmm.michalski@gmail.com>
2023-10-07 11:46:42 +0200

    update

---
+++ arytmetyka.py
@@ -0,0 +1,12 @@
+# Arytmetyka - działania matematyczne na liczbach
+
+# Działania na liczbach całkowitych i zmiennoprzecinkowych
+
+a = 5
+b = 2
+
+print(f"Dodawanie: {a} + {b} = {a + b}")
+print(f"Odejmowanie: {a} - {b} = {a - b}")
+print(f"Mnożenie: {a} * {b} = {a * b}")
+print(f"Dzielenie: {a} / {b} = {a / b}") # Wynikiem dzielenia jest zawsze Float
+
---
+++ typy_danych.py
@@ -0,0 +1,15 @@
+# Typy danych w Pythonie
+
+# Liczba całkowita - Integer
+liczba_calkowita = 13
+
+# Liczba zmiennoprzecinkowa - Floating Point
+liczba_zmiennoprzecinkowa = 0.5
+
+# Typ logiczny - Boolean
+typ_logiczny = False # | True
+
+# Struktury danych
+
+# Napis - String - Łańcuch znaków
+napis = "Ala ma kota"
