a52d10aca0377c9f2649dd8182a8c7b1b73a0c44
dlt-v <tmm.michalski@gmail.com>
2023-10-14 16:18:14 +0200

    update

--- 09 - pygame/wstep.py
+++ 09 - pygame/wstep.py
@@ -1,14 +1,36 @@
 import pygame
 
+
+
 pygame.init() #inicjalizacja pygame
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600 # deklaracja wymiarów okienka
 
+
 screen_surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) #stworzenie okienka
 
+def load_image(img_path, position): # Ładowanie do gry obrazków
+    image = pygame.image.load(img_path)
+    surface = image.convert()
+
+    transparent_color = (0, 0, 0)
+    surface.set_colorkey(transparent_color)
+
+    rect = surface.get_rect(center = position)
+
+    return [image, surface, rect]
+
+def print_image(img_list):
+    image, surface, rect = img_list
+    screen_surface.blit(surface, rect)
+
 pygame.display.set_caption("Pierwsza gra") # ustawiamy napis okna
+
 clock = pygame.time.Clock()
+
+player_position = [SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2]
+player = load_image('images/player.png', player_position)
 
 game_status = True
 
@@ -21,7 +43,7 @@ while game_status: # główna pętla gry
 
         if event.type == pygame.QUIT: # jeżeli klikniemy na X w rogu okna to wychodzimy z pętli
             game_status = False
-
+    print_image(player)
     pygame.display.update() # aktualizujemy ekran
 
     clock.tick(60)
