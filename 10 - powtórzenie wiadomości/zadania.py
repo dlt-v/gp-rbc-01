fc2eb1aeb55fabc51904d7f4b01a07d7a1278948
dlt-v <tmm.michalski@gmail.com>
2023-10-15 11:53:30 +0200

    add tasks

---
+++ 10 - powtórzenie wiadomości/zadania.py
@@ -0,0 +1,71 @@
+"""
+1. Utwórz trzy zmienne, do których wpisz wartość 3 jako odpowiedni typ:
+- x_int - jako liczba całkowita
+- x_float - jako liczba z przecinkiem
+- x_str - jako napis
+
+======================================================================
+
+2. Utwórz zmienną napis_liczba, która przechowuje wartość "290".
+Utwórz zmienną x. Użyj konwersji z typu str na typ int, aby zmienna x
+przechowywała to co napis_liczba, ale jako typ liczby całkowitej
+
+======================================================================
+
+3. Utwórz 3 zmienne:
+- pole_trojkata
+- podstawa_trojkata
+- wysokosc_trojkata
+Do podstawa_trojkata oraz wysokosc_trojkata powinny trafić wartości odczytane z
+konsoli (od użytkownika).
+Oblicz pole takiego trójkąta i zapisz wynik w zmiennej pole_trojkata
+Wyświetl wynik jako komunikat:
+
+Pole trójkąta o podstawie XX oraz wysokości XX wynosi XX
+
+Zamiast XX powinny pojawić się wartości liczbowe
+
+======================================================================
+
+4. Zapytaj użytkownika o jego wiek i na tej podstawie wyświetla w konsoli jeden z
+komunikatów:
+- Jesteś pełnoletni/a
+- Nie jesteś jeszcze pełnoletni/a. Brakuje Ci XX lat do 18 roku życia
+Zamiast XX powinna pojawić się wartość liczbowa
+
+======================================================================
+
+5. Cena atrakcji turystycznej zależy od miesiąca. Napisz program, który zapyta
+użytkownika o liczbę biletów oraz miesiąc, w którym chce odwiedzić park
+rozrywki i na tej podstawie obliczy koszt transakcji.
+Koszt biletu w danym miesiącu (miesiąc jako numer -> koszt biletu):
+- 1 -> 50 zł
+- 2 -> 50 zł
+- 3 -> 100 zł
+- 4 -> 100 zł
+- 5 -> 200 zł
+- 6 -> 200 zł
+- 7 -> 250 zł
+- 8 -> 200 zł
+
+- 9 -> 200 zł
+- 10 -> 100 zł
+- 11 -> 100 zł
+- 12 -> 50 zł
+Wyświetl komunikat:
+"Cena biletów: XX zł"
+XX to wartość liczbowa
+
+Jeśli wprowadzono niepoprawny numer miesiąc program powinien wyświetlić
+informację:
+"Wprowadzono niepoprawny numer miesiąca. Spróbuj ponownie"
+
+======================================================================
+
+6. Napisz program, który zapyta użytkownika o liczbę, a następnie wypisze na
+ekranie tyle wyników z rzutu kością sześcienną.
+Rzut kością sześcienną to wynik z losowania liczby od 1 do 6 (włącznie).
+
+======================================================================
+
+"""
