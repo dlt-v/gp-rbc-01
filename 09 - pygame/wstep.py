47053483ab4d94c4bd0ddfc0714e751b42dc6d45
dlt-v <tmm.michalski@gmail.com>
2023-10-15 11:34:05 +0200

    update

--- 09 - pygame/wstep.py
+++ 09 - pygame/wstep.py
@@ -1,11 +1,12 @@
 import pygame
-
+import random 
 
 
 pygame.init() #inicjalizacja pygame
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600 # deklaracja wymiarów okienka
+FRAMES_PER_SECOND = 60
 
 
 screen_surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) #stworzenie okienka
@@ -45,6 +46,20 @@ def calculate_player_movement(keys):
         delta_x += speed
     return [delta_x, delta_y]
 
+def limit_position(position):
+    """
+    Tworzymy funkcję limit_position, która przyjmuje współrzędne i sprawdza, czy są poprawne. Jeśli nie są poprawne zmienia współrzędne, tak aby zawsze były na ekranie.
+    
+    """
+    x = position[0]
+    y = position[1]
+
+    x = max(0, min(x, SCREEN_WIDTH))
+    y = max(0, min(y, SCREEN_HEIGHT))
+
+
+    return [x, y]
+
 pygame.display.set_caption("Pierwsza gra") # ustawiamy napis okna
 
 clock = pygame.time.Clock()
@@ -52,13 +67,49 @@ clock = pygame.time.Clock()
 player_position = [SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2]
 player = load_image('images/player.png', player_position)
 
-new_item = load_image('images/bonus_1.png', [SCREEN_WIDTH - 20, SCREEN_HEIGHT - 20])
+bonus_images = [ #lista ścieżek do plików
+    'images/bonus_1.png', 
+    'images/bonus_2.png', 
+    'images/bonus_3.png'
+]
+
+bonus_objects = []
+
+def generate_bonus_object():
+    image_path = random.choice(bonus_images)
+
+    x = random.randint(0, SCREEN_WIDTH)
+    y = random.randint(0, SCREEN_HEIGHT)
+
+    new_object = load_image(image_path, [x, y])
+    bonus_objects.append(new_object)
+
+def print_bonus_objects():
+
+    for object in bonus_objects:
+        print_image(object)
+
+def check_collisions():
+    rect_player = player[2]
+
+    # Przeiteruj przez listę bonusowych obiektów
+    for bonus_object in bonus_objects:
+        rect_object = bonus_object[2]
+    # Sprawdź czy gracz dotyka obiektu
+    # Obiekt usuń z listy obiektów jeżeli tak
+        if rect_object.colliderect(rect_player):
+            bonus_objects.remove(bonus_object)
+
+
+    
 
 background_color = [0, 0, 0]
 
 game_status = True
+frames_counter = 0
 
 while game_status: # główna pętla gry
+    frames_counter += 1
 
     events = pygame.event.get() # zbieranie zdarzeń
 
@@ -76,13 +127,20 @@ while game_status: # główna pętla gry
     player_position[0] += delta_x
     player_position[1] += delta_y
 
+    player_position = limit_position(player_position)
+
     player = set_position_image(player, player_position)
 
     print_image(player)
-    # print_image(new_item)
+
+    if frames_counter % (FRAMES_PER_SECOND * 4) == 0:
+        generate_bonus_object()
+    check_collisions()
+    print_bonus_objects()
+
     pygame.display.update() # aktualizujemy ekran
 
-    clock.tick(60)
+    clock.tick(FRAMES_PER_SECOND)
 
 pygame.quit() # wychodzimy
 quit()
